edb5b688a5d7d84284220a0e87f779c34e13337d
Inopiu <95720199+Inopiu@users.noreply.github.com>
2022-02-03 16:33:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `!`;
-exports.Token = `OTM4ODE3NDE3ODYwMjI3MDcy.Yfvz2g.1fIPtdMde-vnWo60jOsKGoqQT3s `;
+exports.Token 'OTM4ODE3NDE3ODYwMjI3MDcy.Yfvz2g.7nv-Kgeiv4H5jJPFQfbadNqhbQs `;
 exports.Color = `RANDOM`;
